372b9d75650373c8cee04f21f47dd5311397a448
iangelmx <aramireza1506@alumno.ipn.mx>
2020-07-23 21:07:48 -0500

    Adding titles to plot
    Searching #clarovideo

--- ploting_with_dash_N3_L2/ploting_tweets.py
+++ ploting_with_dash_N3_L2/ploting_tweets.py
@@ -14,6 +14,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
+import datetime
 import plotly.express as px
 import pandas as pd
 import sys
@@ -24,31 +25,27 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-#df = pd.read_csv('religion.csv')
-
 bd = Bd('localhost', 'aramirez', 'iangelmx', 'test')
-collection = bd.get_docs( 'tweets_alkapone' )
+
+BASE = 'tweets_claro'
+
+collection = bd.get_docs( BASE )
 df = pd.DataFrame.from_records( collection )
 
-
-animals=['giraffes', 'orangutans', 'monkeys']
-
-fig = go.Figure(data=[
-    go.Bar(name='SF Zoo', x=animals, y=[20, 14, 23]),
-    go.Bar(name='LA Zoo', x=animals, y=[12, 18, 29])
-])
-# Change the bar mode
-fig.update_layout(barmode='group')
+df['created_at'] = df['created_at'].apply( lambda fecha: str(fecha) )
+df['text'] = df['text'].apply(lambda x: x.replace('.', '.\n<br>').replace('?', '?\n<br>').replace('!', '!\n<br>').replace('y', 'y\n<br>') )
 
 fig = px.scatter(df, x="likes", y="rts",
-                 #size="Rango_Edades",
-                 #color="Sexo", 
-                 hover_name="text"
-                 #log_x=True, 
-                 #size_max=60
-                 )
+                 hover_data=["text", 'created_at'],
+                 height=600
+)
 
-app.layout = html.Div([
+app.layout = html.Div(children=[
+    html.H1(children='Dash Plot'),
+
+    html.Div(children=f'''
+        Tweets, likes and Retweets... of {BASE}
+    '''),
     dcc.Graph(
         id='religion-mexico',
         figure=fig,
--- sentiment_analysis/save_tweets_to_db.py
+++ sentiment_analysis/save_tweets_to_db.py
@@ -7,7 +7,8 @@ from libs.mongo_lib import Bd
 
 #MYMALK4PON3
 
-tweets_records, en_tweets = get_analytics_for_profile('MYMALK4PON3',limit_tweets=200, translate_to_en=True)
+tweets_records, en_tweets = get_analytics_for_search('#clarovideo',limit_tweets=200, translate_to_en=True)
+#tweets_records, en_tweets = get_analytics_for_profile('MYMALK4PON3',limit_tweets=200, translate_to_en=True)
 
 #Get the records with original and translated tweet
 records = merge_tweets_to_translated_text( tweets_records, en_tweets )
@@ -19,4 +20,4 @@ for a in records:
     print("\n\n\n\n")
 
 
-print(bd.insert_in_db('tweets_alkapone', list(records)))
+print(bd.insert_in_db('tweets_claro', list(records)))
